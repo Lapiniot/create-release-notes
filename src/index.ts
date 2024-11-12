0224d120174d4018ad52fa4d9c61dbd03013a061
Lapiniot <aossss@gmail.com>
2024-11-12 15:33:29 +0200

    Initial viable implementation draft

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { getInput, info, setFailed, setOutput, warning } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
-import fs from "node:fs/promises";
+import YAML from "../ts-yaml/src/Parser";
+import defaultConfig from "./release.json";
 
 type Unwrap<T> = T extends Promise<infer P> ? P : T;
-type ArrayType<T> = T extends Array<infer P> ? P : T;
-type PageResult<T> = [data: T[], hasMore: boolean];
-
 type OctokitClient = ReturnType<typeof getOctokit>;
-type Commit = ArrayType<Unwrap<ReturnType<OctokitClient["rest"]["repos"]["listCommits"]>>["data"]>;
 type Issue = Unwrap<ReturnType<OctokitClient["rest"]["issues"]["get"]>>["data"];
+type Config = {
+    changelog: {
+        exclude?: {
+            labels?: string[],
+            authors?: string[]
+        },
+        categories: {
+            title: string,
+            labels: string[],
+            exclude?: Config["changelog"]["exclude"]
+        }[]
+    }
+}
 
 async function run() {
     try {
@@ -32,6 +42,36 @@ async function run() {
         }
 
         const params = { owner, repo, per_page: 100 };
+
+        let config: Config = defaultConfig;
+
+        for (const path of getConfigPaths()) {
+            try {
+                const { data } = await client.rest.repos.getContent({ ...params, path });
+                const content = "type" in data && data.type === "file" ? atob(data.content) : "";
+                config = YAML.parse(content);
+                break;
+            } catch (error: any) {
+                if (error.status !== 404) {
+                    throw error;
+                }
+            }
+        }
+
+        const excludeLabels = new Set(config.changelog.exclude?.labels ?? []);
+        const excludeAuthors = new Set(config.changelog.exclude?.authors?.map(a => a.toLowerCase()) ?? []);
+        const excludePredicate = (label: string) => excludeLabels.has(label);
+
+        const categories = config.changelog.categories.map(
+            ({ title, labels = [], exclude: { labels: excludeLabels = [], authors: excludeAuthors = [] } = {} }) =>
+            ({
+                title, labels,
+                excludeLabels: new Set(excludeLabels),
+                excludeAuthors: new Set(excludeAuthors.map(ea => ea.toLowerCase())),
+                issues: new Array<Issue>()
+            }));
+        const labelMappings = new Map(categories.flatMap(category => category.labels.map(label => [label, category])));
+        const catchAllCategory = labelMappings.get("*");
 
         const fetchCommits = prevTag
             ? async function* () {
@@ -67,21 +107,70 @@ async function run() {
             }
         }
 
-        let markup = "";
-        for (let [_, { title, state, html_url, assignees }] of issues) {
+        for (const [_, issue] of issues) {
+            const { state, labels, closed_by } = issue;
             if (state === "closed") {
-                const assigneesList = assignees?.map(({ login, html_url }) =>
-                    `[@${login}](${html_url})`)
-                    .join(", ");
-                markup += ` - [${title}](${html_url}) (${assigneesList})\n`;
+                const closedBy = closed_by!.login.toLowerCase();
+                const lbls = labels.map(lbl => typeof lbl === "string" ? lbl : lbl.name!);
+
+                if (lbls.some(excludePredicate) || excludeAuthors.has(closedBy)) {
+                    continue;
+                }
+
+                let mapped = false;
+
+                for (const label of lbls) {
+                    const category = labelMappings.get(label);
+                    if (category &&
+                        !lbls.some(lbl => category.excludeLabels.has(lbl)) &&
+                        !category.excludeAuthors.has(closedBy)) {
+                        category.issues.push(issue);
+                        mapped = true;
+                    }
+                }
+
+                if (catchAllCategory && !mapped && !catchAllCategory.excludeAuthors.has(closedBy)) {
+                    catchAllCategory.issues.push(issue);
+                }
             }
         }
 
-        console.log(markup);
+        let markup = "";
+        for (const { title, issues } of categories) {
+            if (issues.length === 0)
+                continue;
+
+            markup += `### ${title}\n`;
+
+            for (const { title, number, assignee, html_url } of issues) {
+                const eventIterator = client.paginate.iterator(client.rest.issues.listEvents, { ...params, issue_number: number });
+                const commiters = new Set<string>();
+                for await (const { data } of eventIterator) {
+                    for (const { event, commit_id, actor: { login } } of data) {
+                        if (commit_id && (event === "referenced" || event === "closed" || event === "commited")) {
+                            commiters.add(login);
+                        }
+                    }
+                }
+
+                const contributors = commiters.size > 0 ? Array.from(commiters).map(c => `@${c}`).join(", ") : assignee;
+                markup += ` - [${title}](${html_url}) (${contributors})\n`;
+            }
+        }
 
         setOutput("release_notes_content", markup);
     } catch (error) {
         setFailed((error as Error).message);
+    }
+}
+
+function* getConfigPaths() {
+    const configPath = getInput("configuration_file_path");
+    if (configPath) {
+        yield configPath;
+    } else {
+        yield ".github/release.yml";
+        yield ".github/release.yaml";
     }
 }
 
